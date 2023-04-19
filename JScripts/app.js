cca94bee62e1930ac11f7bc8e5862b2aeb510dca
João Victor <116379577+maspoxajoao@users.noreply.github.com>
2023-04-19 20:40:06 -0300

    commit funções de edit e delete

--- JScripts/app.js
+++ JScripts/app.js
@@ -1,5 +1,5 @@
-//Aqui guardo a informação para a API
-
+// Função assíncrona que faz uma requisição POST para a API
+// para adicionar um usuário ao banco de dados
 const addApi = async (usuario) => {
   await fetch("http://localhost:3000/users", {
     method: "POST",
@@ -11,22 +11,22 @@ const addApi = async (usuario) => {
   });
 };
 
-const formularioCadastro = document.getElementById("form");
-
-if (formularioCadastro) {
-  formularioCadastro.addEventListener("submit", async (e) => {
+// Seleciona o formulário de cadastro e adiciona um listener de evento
+if (document.getElementById("form")) {
+  document.getElementById("form").addEventListener("submit", async (e) => {
     e.preventDefault();
-    //Aqui eu Pego a informação do usuario e trago valores para a API em forma de objeitos
+    
+    // Obtém os valores do formulário de cadastro
     const user = {
       name: document.getElementById("txNome")?.value,
       email: document.getElementById("txEmail").value,
       senha: document.getElementById("txSenha").value,
-      paciente: [],
     };
 
-    //faço confirmação de senha com true e false
+    // Confirmação de senha
     const confSenha = document.getElementById("confSenha")?.value;
     if (user.senha === confSenha) {
+      // Adiciona o usuário na API e redireciona para a página inicial
       await addApi(user);
       window.location = "/indexs/index.html";
     } else {
@@ -35,33 +35,36 @@ if (formularioCadastro) {
   });
 }
 
-function next() {
-  const div1 = (document.getElementById("div1").style.display = "none");
-  const div2 = (document.getElementById("div2").style.display = "block");
-  console.log("rodou");
-}
-
-const formularioLogin = document.getElementById("formulario");
-
-if (formularioLogin) {
-  formularioLogin.addEventListener("submit", async (e) => {
+// Seleciona o formulário de login e adiciona um listener de evento
+if (document.getElementById("formulario")) {
+  document.getElementById("formulario").addEventListener("submit", async (e) => {
     e.preventDefault();
+    
+    // Obtém os valores do formulário de login
     const email = document.getElementById("txEmail").value;
-
-    const apiResponse = await fetch(
-      `http://localhost:3000/users?email=${email}`
-    );
-    const users = await apiResponse.json();
-
     const senha = document.getElementById("txSenha").value;
 
+    // Faz uma requisição GET para a API buscando um usuário com o email fornecido
+    const apiResponse = await fetch(`http://localhost:3000/users?email=${email}`);
+    const users = await apiResponse.json();
+
+    // Verifica se o email e senha são válidos
+    let valid = false;
     users.forEach((user) => {
-      if (email === user.email && senha === user.senha)
+      if (email === user.email && senha === user.senha) {
+        valid = true;
         window.location = "/indexs/pacientes.html";
-      else {
-        window.alert("[ERRO] Email ou senha incorreto!");
       }
     });
+
+    if (!valid) {
+      window.alert("[ERRO] Email ou senha incorreto!");
+    }
   });
 }
 
+// Função que esconde a primeira div e mostra a segunda
+function next() {
+  document.getElementById("div1").style.display = "none";
+  document.getElementById("div2").style.display = "block";
+}
--- JScripts/nav.js
+++ JScripts/nav.js
@@ -1,50 +1,72 @@
+// Abre o modal na página
 function openModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "block";
-  }
-  
-  function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-  }
-  
-  const addPaciente = async (paciente) => {
-    await fetch("http://localhost:3000/pacientes", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(paciente),
-    });
-  };
-  
-  const formularioModal = document.getElementById("formularioModal");
-  
-  if (formularioModal) {
-    formularioModal.addEventListener("submit", async (e) => {
-      e.preventDefault();
-      console.log("teste")
-      // Aqui eu pego a informação dos pacientes no HTML e trago valores para a API em forma de objetos
-      const paciente = {
-        CPF: document.getElementById("cpf").value,
-        nome: document.getElementById("nome").value,
-        dataNascimento: document.getElementById("dataNasc").value,
-        email: document.getElementById("email").value,
-        sexoGenero: document.getElementById("sexoGenero").value,
-        nacionalidade: document.getElementById("nacionalidade").value,
-        naturalidade: document.getElementById("naturalidade").value,
-        profissao: document.getElementById("profissao").value,
-        escolaridade: document.getElementById("escolaridade").value,
-        estadoCivil: document.getElementById("estadoCivil").value,
-        mae: document.getElementById("mae").value,
-        pai: document.getElementById("pai").value,
-      };
-      
-      // Envia os dados do paciente para a API
-      await addPaciente(paciente);
-      
-      // Fecha o modal após o envio dos dados
-      closeModal();
-    });
-  }
+  const modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+
+// Fecha o modal na página
+function closeModal() {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+// Envia os dados do paciente para a API
+const addPaciente = async (paciente) => {
+  await fetch("http://localhost:3000/pacientes", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(paciente),
+  });
+};
+
+// Edita os dados de um paciente na API
+const editaPaciente = async (id, paciente) => {
+  await fetch(`http://localhost:3000/pacientes/${id}`, {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(paciente),
+  });
+};
+
+// Deleta um paciente da API
+const deletaPaciente = async (id) => {
+  await fetch(`http://localhost:3000/pacientes/${id}`, {
+    method: "DELETE",
+  });
+};
+
+// Adiciona um listener para o evento de submit do formulário do modal
+const formularioModal = document.getElementById("formularioModal");
+if (formularioModal) {
+  formularioModal.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    // Coleta os dados dos campos do formulário do modal
+    const paciente = {
+      CPF: document.getElementById("cpf").value,
+      nome: document.getElementById("nome").value,
+      dataNascimento: document.getElementById("dataNasc").value,
+      email: document.getElementById("email").value,
+      sexoGenero: document.getElementById("sexoGenero").value,
+      nacionalidade: document.getElementById("nacionalidade").value,
+      naturalidade: document.getElementById("naturalidade").value,
+      profissao: document.getElementById("profissao").value,
+      escolaridade: document.getElementById("escolaridade").value,
+      estadoCivil: document.getElementById("estadoCivil").value,
+      mae: document.getElementById("mae").value,
+      pai: document.getElementById("pai").value,
+    };
+
+    // Envia os dados do paciente para a API através da função addPaciente()
+    await addPaciente(paciente);
+
+    // Fecha o modal após o envio dos dados
+    closeModal();
+  });
+}
